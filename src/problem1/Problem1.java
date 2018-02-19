3624c4bd250219462db0616852e7a571b864a40c
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-02-19 23:27:36 +0000

    Add problem 1

---
+++ src/problem1/Problem1.java
@@ -0,0 +1,19 @@
+package problem1;
+
+public class Problem1 {
+    public static int getSumOfFactors3And5(int upperBound) {
+        int sum = 0;
+        
+        for (int i = 1; i < upperBound; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getSumOfFactors3And5(1000));
+    }
+}
