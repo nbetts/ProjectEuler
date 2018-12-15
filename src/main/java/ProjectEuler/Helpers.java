09b503599e33daa7be3822798b24f6829b246b13
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-15 20:13:16 +0000

    Update readFileNumbers2D method to always enough columns

--- src/main/java/ProjectEuler/Helpers.java
+++ src/main/java/ProjectEuler/Helpers.java
@@ -80,10 +80,21 @@ public class Helpers {
   public static int[][] readFileNumbers2D(String filename) {
     try {
       List<String> lines = Files.readAllLines(Paths.get(filename));
-      int[][] array = new int[lines.size()][lines.get(0).split(" ").length];
+      int maxArrayLength = lines.get(0).split(" ").length;
+
+      for (String line : lines) {
+        int arrayLength = line.split(" ").length;
+
+        if (arrayLength > maxArrayLength) {
+          maxArrayLength = arrayLength;
+        }
+      }
+
+      int[][] array = new int[lines.size()][maxArrayLength];
 
       for (int i = 0; i < lines.size(); i++) {
         String[] numbers = lines.get(i).split(" ");
+        System.out.println(numbers.length);
 
         for (int j = 0; j < numbers.length; j++) {
           array[i][j] = Integer.parseInt(numbers[j]);
