f764fb499d74e38d7cd62e88ce4568f4f3fbfb8d
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-15 17:51:27 +0000

    Add problem 9

---
+++ src/main/java/ProjectEuler/Problem9.java
@@ -0,0 +1,18 @@
+package ProjectEuler;
+
+public class Problem9 implements Solvable {
+  public String solve() {
+    for (int a = 1; a < 1000; a++) {
+      for (int b = a + 1; b < 1000; b++) {
+        double squareSum = (double)(a * a + b * b);
+        int c = (int) Math.sqrt(squareSum);
+
+        if (c * c == squareSum && a + b + c == 1000) {
+          return Integer.toString(a * b * c);
+        }
+      }
+    }
+
+    return "0";
+  }
+}
