1fafb556aa23567696e6aaa965c8b5d3aa5dd4d5
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-15 09:29:40 +0000

    Add problem 2

---
+++ src/main/java/ProjectEuler/Problem2.java
@@ -0,0 +1,22 @@
+package ProjectEuler;
+
+public class Problem2 implements Problem {
+  public String solve() {
+    int upperBound = 4000000;
+    int previousFibonacciValue = 0;
+    int currentFibonacciValue = 1;
+    int temp, sum = 0;
+
+    while (currentFibonacciValue < upperBound) {
+      temp = currentFibonacciValue;
+      currentFibonacciValue = currentFibonacciValue + previousFibonacciValue;
+      previousFibonacciValue = temp;
+
+      if ((currentFibonacciValue & 1) == 0) {
+        sum += currentFibonacciValue;
+      }
+    }
+
+    return Integer.toString(sum);
+  }
+}
