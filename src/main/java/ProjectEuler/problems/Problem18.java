a5c83c50b35733faedaeb5f8dfb240ca4ae72c4b
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-19 19:19:09 +0000

    Optimise problem 18

--- src/main/java/ProjectEuler/problems/Problem18.java
+++ src/main/java/ProjectEuler/problems/Problem18.java
@@ -4,7 +4,7 @@ public class Problem18 implements Solvable {
   private int[][] numbers;
   private int largestSum = 0;
 
-  private void checkSum(int yOffset, int xOffset, int sum) {
+  private void checkSum(int yOffset, int xOffset, int sum, boolean checkLeft, boolean checkRight) {
     if (yOffset == numbers[0].length - 1) {
       if (sum > largestSum) {
         largestSum = sum;
@@ -13,13 +13,21 @@ public class Problem18 implements Solvable {
       return;
     }
 
-    checkSum(yOffset + 1, xOffset, sum + numbers[yOffset + 1][xOffset]);
-    checkSum(yOffset + 1, xOffset + 1, sum + numbers[yOffset + 1][xOffset + 1]);
+    int left = numbers[yOffset + 1][xOffset];
+    int right = numbers[yOffset + 1][xOffset + 1];
+    boolean checkOverlap = left > right;
+
+    if (checkLeft) {
+      checkSum(yOffset + 1, xOffset, sum + left, true, checkOverlap);
+    }
+    if (checkRight) {
+      checkSum(yOffset + 1, xOffset + 1, sum + right, !checkOverlap, true);
+    }
   }
 
   public String solve() {
     numbers = FileHelpers.readFileNumbers2D("resources/Problem18.txt");
-    checkSum(0, 0, numbers[0][0]);
+    checkSum(0, 0, numbers[0][0], true, true);
 
     return Integer.toString(largestSum);
   }
