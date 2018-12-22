5fa40945aa935cde5947e8dfe8d341743313fc16
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-22 23:11:12 +0000

    Rename check method to checkCoins in problem 31

--- src/main/java/ProjectEuler/problems/Problem31.java
+++ src/main/java/ProjectEuler/problems/Problem31.java
@@ -4,7 +4,7 @@ public class Problem31 implements Solvable {
   private int[] coins = { 200, 100, 50, 20, 10, 5, 2, 1 };
   private int answer = 0;
 
-  private void check(int index, int remaining) {
+  private void checkCoins(int index, int remaining) {
     if (remaining == 0) {
       answer++;
       return;
@@ -13,12 +13,12 @@ public class Problem31 implements Solvable {
     }
 
     for (int i = index; i < coins.length; i++) {
-      check(i, remaining - coins[i]);
+      checkCoins(i, remaining - coins[i]);
     }
   }
 
   public String solve() {
-    check(0, 200);
+    checkCoins(0, 200);
 
     return Integer.toString(answer);
   }
