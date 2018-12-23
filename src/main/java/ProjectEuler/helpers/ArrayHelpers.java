22cae01e9456dedc1430c0a91c2309cf6647e4be
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-23 00:57:22 +0000

    Add isPalindrome method in ArrayHelpers

--- src/main/java/ProjectEuler/helpers/ArrayHelpers.java
+++ src/main/java/ProjectEuler/helpers/ArrayHelpers.java
@@ -25,4 +25,10 @@ public class ArrayHelpers {
     return number.toString();
   }
 
+  /**
+   * Return true if a string is a palindrome.
+   */
+  public static boolean isPalindrome(String string) {
+    return string.equals(new StringBuilder(string).reverse().toString());
+  }
 }
--- src/main/java/ProjectEuler/problems/Problem4.java
+++ src/main/java/ProjectEuler/problems/Problem4.java
@@ -1,23 +1,24 @@
 package ProjectEuler;
 
 public class Problem4 implements Solvable {
+  public static boolean isPalindrome(String string) {
+    for (int i = 0; i < string.length() / 2; i++) {
+      if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   public String solve() {
     int answer = 0;
 
     for (int i = 999; i > 99; i--) {
       for (int j = i; j > 99; j--) {
         int product = i * j;
-        String string = Integer.toString(product);
-        boolean isPalindrome = true;
 
-        for (int k = 0; k < string.length()/2; k++) {
-          if (string.charAt(k) != string.charAt(string.length()-1-k)) {
-            isPalindrome = false;
-            break;
-          }
-        }
-
-        if (isPalindrome && product > answer) {
+        if (product > answer && ArrayHelpers.isPalindrome(Integer.toString(product))) {
           answer = product;
         }
       }
