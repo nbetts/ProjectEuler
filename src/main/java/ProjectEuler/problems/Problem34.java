1b16903161281f0b394a9780375dd8751801193a
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-12-23 00:49:07 +0000

    Add problem 34

---
+++ src/main/java/ProjectEuler/problems/Problem34.java
@@ -0,0 +1,31 @@
+package ProjectEuler;
+
+import java.util.Collections;
+
+public class Problem34 implements Solvable {
+  public String solve() {
+    int answer = 1;
+
+    for (int i = 3; i < 1000000; i += 2) {
+      if (MathHelpers.isPrime(i)) {
+        String number = Integer.toString(i);
+        boolean isCircularPrime = true;
+
+        for (int j = 0; j < number.length() - 1; j++) {
+          number = number.substring(1) + number.substring(0, 1);
+
+          if (!MathHelpers.isPrime(Integer.parseInt(number))) {
+            isCircularPrime = false;
+            break;
+          }
+        }
+
+        if (isCircularPrime) {
+          answer++;
+        }
+      }
+    }
+
+    return Integer.toString(answer);
+  }
+}
